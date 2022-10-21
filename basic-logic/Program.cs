c2b42bb32b295130b2fb67d26812963637dac2a8
Damaris <dmagallonestrada@gmail.com>
2022-10-21 16:52:27 -0500

    Ejemplo de mayor de tres números

---
+++ basic-logic/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace basic_logic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe el número 1");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escribe el segundo número");
+            int n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Escribe el número 1");
+            int n3 = int.Parse(Console.ReadLine());
+
+            int tmp = 0;
+
+            if (n1 > n2)
+            {
+                tmp = n1;
+            }
+            else
+                tmp = n2;
+
+            if (tmp < n3)
+                tmp = n3;
+
+                Console.WriteLine("El número " + tmp + " es mayor.");
+
+        }
+    }
+}
